4e9b51cc492cd5c608b8b301d25cf5ad471d4c59
upceric <upcliweiwei@gmail.com>
2019-03-10 23:25:27 +0100

    change read

--- read.py
+++ read.py
@@ -2,12 +2,16 @@ from matplotlib import pyplot as plt
 import numpy as np
 #image dimension is 2332*2240
 image=np.empty((2332,2240),np.uint16)
+#define the filename
+filename='60KV_100mmA_1s_1avg_06_03_2019_fdd739370_fod450.raw'
 #read image to A, filetype is unsigned 16
-A=np.fromfile('60KV_100mmA_1s_1avg_06_03_2019_fdd739370_fod450.raw',dtype='uint16',sep="")
+A=np.fromfile(filename,dtype='uint16',sep="")
 #skip header
 B=A[1024:]
+#reshape the matrix with the dimension of the picture 
 B=B.reshape([2240,2332])
 #choose a roi region and claculate the mean value
-i0=np.mean(B[0:100,0:100])
+#i0=np.mean(B[0:100,0:100])
+#plot the image
 plt.imshow(B,cmap='gray')
 plt.show()
